1c5f9662ab7dbff94a6a0bc36461d3e02e9092c4
Aakash9033 <aakashvermartk@gmail.com>
2024-05-20 15:54:50 +0530

    created Signup,login and logout api with user authentication using jwt token using middleware to authticate also created message and conversation model successfully

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 const app = express();
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./DB/connectToMongoDB.js";
-const PORT = process.env.PORT || 5000;
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World testing server!");
